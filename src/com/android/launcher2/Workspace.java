54436fb44f3ea661de4176ed8a8a402746f7a154
Danesh M <daneshm90@gmail.com>
2011-12-14 21:01:43 -0500

    Workspace: Resize Widget Restriction
    Only allow widget to be resized if picked/dropped in same location.
    
    Change-Id: I2a7be6749608aa8b66a164fb1472d8aa395f237f

--- src/com/android/launcher2/Workspace.java
+++ src/com/android/launcher2/Workspace.java
@@ -2235,7 +2235,10 @@ public class Workspace extends SmoothPagedView
 
                         final LauncherAppWidgetHostView hostView = (LauncherAppWidgetHostView) cell;
                         AppWidgetProviderInfo pinfo = hostView.getAppWidgetInfo();
-                        if (pinfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE || mResizeAnyWidget) {
+                        boolean hasMovedCells = ((mTargetCell[0] != mDragInfo.cellX) || (mTargetCell[1] != mDragInfo.cellY));
+
+                        if ((pinfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE || mResizeAnyWidget)
+                                && !hasMovedCells && !hasMovedLayouts) {
                             final Runnable resizeRunnable = new Runnable() {
                                 public void run() {
                                     DragLayer dragLayer = mLauncher.getDragLayer();
