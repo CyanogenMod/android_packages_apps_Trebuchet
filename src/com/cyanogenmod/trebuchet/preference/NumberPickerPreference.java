cbe0a6fd957f83218bf46ee405c5101360cd29f2
nebkat <nebkat@teamhacksung.org>
2012-01-16 08:04:02 +0000

    NumberPickerPreference: Fix possible NPE
    Change-Id: I0dc999b8a782a2a4038aecbfc450b007f0272126

--- src/com/cyanogenmod/trebuchet/preference/NumberPickerPreference.java
+++ src/com/cyanogenmod/trebuchet/preference/NumberPickerPreference.java
@@ -100,14 +100,14 @@ public class NumberPickerPreference extends DialogPreference {
         mNumberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
 
         if (mNumberPicker == null) {
-            throw new RuntimeException("NumberPicker is null!");
+            throw new RuntimeException("mNumberPicker is null!");
         }
 
         // Initialize state
+        mNumberPicker.setWrapSelectorWheel(false);
         mNumberPicker.setMaxValue(max);
         mNumberPicker.setMinValue(min);
         mNumberPicker.setValue(getPersistedInt(mDefault));
-        mNumberPicker.setWrapSelectorWheel(false);
 
         // No keyboard popup
         EditText textInput = (EditText) mNumberPicker.findViewById(com.android.internal.R.id.numberpicker_input);
