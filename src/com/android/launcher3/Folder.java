48589f73feca7213df5d36648910ae1d521f9cf7
Danesh M <daneshm90@gmail.com>
2014-07-29 17:37:22 -0700

    Trebuchet : Add folder on folder checks
    - Check if the new folder can accomodate all items
    - Don't expand folder if trying to drop folder on folder
    
    Change-Id: I208e3afd4ee6aeeea01928fc77e136d6085c4fdc

--- src/com/android/launcher3/Folder.java
+++ src/com/android/launcher3/Folder.java
@@ -1073,6 +1073,10 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         return getItemCount() >= mMaxNumItems;
     }
 
+    public int getMaxItems() {
+        return mMaxNumItems;
+    }
+
     private void centerAboutIcon() {
         DragLayer.LayoutParams lp = (DragLayer.LayoutParams) getLayoutParams();
 
