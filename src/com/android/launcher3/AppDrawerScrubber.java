c8bed899e5ac84bbac52ae51b07f956e74cd7843
Linus Lee <llee@cyngn.com>
2015-03-24 17:15:10 -0700

    App drawer: add animations and letters to make things pretty
    Change-Id: Iaf6718893e09df715b4524b864be9a7c22addc59
    (cherry picked from commit e002b691b3233629598c77ef50f1ef91eb1877f0)

--- src/com/android/launcher3/AppDrawerScrubber.java
+++ src/com/android/launcher3/AppDrawerScrubber.java
@@ -19,26 +19,30 @@ package com.android.launcher3;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class AppDrawerScrubber extends LinearLayout {
-
-    private final int SCRUBBER_INDICATOR_DISPLAY_DURATION = 200;
-    private final float SCRUBBER_INDICATOR_DISPLAY_TRANSLATIONY = 20f;
-
     private AppDrawerListAdapter mAdapter;
     private RecyclerView mListView;
     private TextView mScrubberIndicator;
     private SeekBar mSeekBar;
-    private String[] mSections;
+    private AutoExpandTextView mScrubberText;
+    private SectionContainer mSectionContainer;
     private LinearLayoutManager mLayoutManager;
+    private ScrubberAnimationState mScrubberAnimationState;
 
     public AppDrawerScrubber(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -50,9 +54,85 @@ public class AppDrawerScrubber extends LinearLayout {
         init(context);
     }
 
+    /**
+     * Simple container class that tries to abstract out the knowledge of complex sections vs
+     * simple string sections
+     */
+    private static class SectionContainer {
+        private ArrayList<AppDrawerScrubberSections> mSections;
+        private String[] mHeaders;
+
+        public SectionContainer(String[] headers) {
+            mSections = AppDrawerScrubberSections.createSections(headers);
+            mHeaders = headers;
+        }
+
+        public int size() {
+            return showLetters() ? mSections.size() : mHeaders.length;
+        }
+
+        public String getHeader(int idx) {
+            return showLetters() ? mSections.get(idx).getText() : mHeaders[idx];
+        }
+
+        /**
+         * Because the list section headers is not necessarily the same size as the scrubber
+         * letters, we need to map from the larger list to the smaller list.
+         * In the case that curIdx is not highlighted, it will use the directional index to
+         * determine the adapter index
+         * @return the mHeaders index (aka the underlying adapter index).
+         */
+        public int getAdapterIndex(int prevIdx, int curIdx) {
+            if (!showLetters()) {
+                return curIdx;
+            }
+
+            // because we have some unhighlighted letters, we need to first get the directional
+            // index before getting the adapter index
+            return mSections.get(getDirectionalIndex(prevIdx, curIdx)).getAdapterIndex();
+        }
+
+        /**
+         * Given the direction the user is scrolling in, return the closest index which is a
+         * highlighted index
+         */
+        public int getDirectionalIndex(int prevIdx, int curIdx) {
+            if (!showLetters() || mSections.get(curIdx).getHighlight()) {
+                return curIdx;
+            }
+
+            if (prevIdx < curIdx) {
+                return mSections.get(curIdx).getNextIndex();
+            } else {
+                return mSections.get(curIdx).getPreviousIndex();
+            }
+        }
+
+        /**
+         * @return true if the scrubber is showing characters as opposed to a line
+         */
+        public boolean showLetters() {
+            return mSections != null;
+        }
+
+        /**
+         * Initializes the scrubber text with the proper characters
+         */
+        public void initializeScrubberText(AutoExpandTextView scrubberText) {
+            scrubberText.setSections(AppDrawerScrubberSections.getHighlightText(mSections));
+        }
+    }
+
     public void updateSections() {
-        mSections = (String[]) mAdapter.getSections();
-        mSeekBar.setMax(mSections.length - 1);
+        mSectionContainer = new SectionContainer((String[]) mAdapter.getSections());
+        mSectionContainer.initializeScrubberText(mScrubberText);
+        mSeekBar.setMax(mSectionContainer.size() - 1);
+
+        // show a white line if there are no letters, otherwise show transparent
+        Drawable d = mSectionContainer.showLetters() ? new ColorDrawable(Color.TRANSPARENT)
+            : getContext().getResources().getDrawable(R.drawable.seek_back);
+        ((ViewGroup)mSeekBar.getParent()).setBackground(d);
+
     }
 
     public void setSource(RecyclerView listView) {
@@ -68,63 +148,128 @@ public class AppDrawerScrubber extends LinearLayout {
     private boolean isReady() {
         return mListView != null &&
                 mAdapter != null &&
-                mSections != null;
+                mSectionContainer != null;
     }
 
     private void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.scrub_layout, this);
+        mScrubberAnimationState = new ScrubberAnimationState();
         mSeekBar = (SeekBar) findViewById(R.id.scrubber);
+        mScrubberText = (AutoExpandTextView) findViewById(R.id.scrubberText);
+        mSeekBar.setOnSeekBarChangeListener(mScrubberAnimationState);
+    }
 
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, final int progress, boolean fromUser) {
-                if (!isReady()) {
-                    return;
+    /**
+     * Handles the animations of the scrubber indicator
+     */
+    private class ScrubberAnimationState implements SeekBar.OnSeekBarChangeListener {
+        private static final long SCRUBBER_DISPLAY_DURATION = 150;
+        private static final float SCRUBBER_SCALE_START = 0f;
+        private static final float SCRUBBER_SCALE_END = 1f;
+        private static final float SCRUBBER_ALPHA_START = 0f;
+        private static final float SCRUBBER_ALPHA_END = 1f;
+
+        private boolean mTouchingTrack = false;
+        private boolean mAnimatingIn = false;
+        private int mLastIndex = -1;
+
+        private void touchTrack(boolean touching) {
+            mTouchingTrack = touching;
+
+            if (mScrubberIndicator != null) {
+                if (mTouchingTrack) {
+                    animateIn();
+                } else if (!mAnimatingIn) { // finish animating in before animating out
+                    animateOut();
                 }
-                resetScrubber();
 
-                String section = String.valueOf(mSections[progress]);
-
-                if (mScrubberIndicator != null) {
-                    float translateX = (progress * seekBar.getWidth()) / mSections.length;
-                    translateX -= (mScrubberIndicator.getWidth() / 6); // offset for alignment
-                    mScrubberIndicator.setTranslationX(translateX);
-                    mScrubberIndicator.setText(section);
-                }
-
-                mLayoutManager.smoothScrollToPosition(mListView, null,
-                        mAdapter.getPositionForSection(progress));
+                mAdapter.setDragging(mTouchingTrack);
             }
+        }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                resetScrubber();
-                if (mScrubberIndicator != null) {
-                    mScrubberIndicator.setAlpha(1f);
-                    mScrubberIndicator.setVisibility(View.VISIBLE);
-                }
-            }
+        private void animateIn() {
+            // start from a scratch position when animating in
+            mScrubberIndicator.animate().cancel();
+            mScrubberIndicator.setPivotX(mScrubberIndicator.getMeasuredWidth() / 2);
+            mScrubberIndicator.setPivotY(mScrubberIndicator.getMeasuredHeight() * 0.8f);
+            mScrubberIndicator.setAlpha(SCRUBBER_ALPHA_START);
+            mScrubberIndicator.setScaleX(SCRUBBER_SCALE_START);
+            mScrubberIndicator.setScaleY(SCRUBBER_SCALE_START);
+            mScrubberIndicator.setVisibility(View.VISIBLE);
+            mAnimatingIn = true;
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                resetScrubber();
-                if (mScrubberIndicator != null) {
-                    mScrubberIndicator.animate().alpha(0f).translationYBy(20f)
-                            .setDuration(200).setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mScrubberIndicator.setVisibility(View.INVISIBLE);
+            mScrubberIndicator.animate()
+                .alpha(SCRUBBER_ALPHA_END)
+                .scaleX(SCRUBBER_SCALE_END)
+                .scaleY(SCRUBBER_SCALE_END)
+                .setDuration(SCRUBBER_DISPLAY_DURATION)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mAnimatingIn = false;
+                        // if the user has stopped touching the seekbar, animate back out
+                        if (!mTouchingTrack) {
+                            animateOut();
                         }
-                    });
-                }
+                    }
+                })
+                .start();
+        }
+
+        private void animateOut() {
+            mScrubberIndicator.animate()
+                .alpha(SCRUBBER_ALPHA_START)
+                .scaleX(SCRUBBER_SCALE_START)
+                .scaleY(SCRUBBER_SCALE_START)
+                .setDuration(SCRUBBER_DISPLAY_DURATION)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mScrubberIndicator.setVisibility(View.INVISIBLE);
+                    }
+                });
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int index, boolean fromUser) {
+            if (!isReady()) {
+                return;
             }
 
-            private void resetScrubber() {
-                if (mScrubberIndicator != null) {
-                    mScrubberIndicator.animate().cancel();
-                    mScrubberIndicator.setTranslationY(0f);
+            if (mScrubberIndicator != null) {
+                // get the index based on the direction the user is scrolling
+                int directionalIndex = mSectionContainer.getDirectionalIndex(mLastIndex, index);
+                String sectionText = mSectionContainer.getHeader(directionalIndex);
+
+                float translateX = (index * seekBar.getWidth()) / (float)mSectionContainer.size();
+                // if we are showing letters, grab the position based on the text view
+                if (mSectionContainer.showLetters()) {
+                    translateX = mScrubberText.getPositionOfSection(index);
                 }
+
+                // center the x position
+                translateX -= mScrubberIndicator.getMeasuredWidth() / 2;
+
+                mScrubberIndicator.setTranslationX(translateX);
+                mScrubberIndicator.setText(sectionText);
             }
-        });
+
+            // get the index of the underlying list
+            int adapterIndex = mSectionContainer.getAdapterIndex(mLastIndex, index);
+            mLayoutManager.smoothScrollToPosition(mListView, null,
+                    mAdapter.getPositionForSection(adapterIndex));
+
+            mLastIndex = index;
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            touchTrack(true);
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            touchTrack(false);
+        }
     }
 }
---
+++ src/com/android/launcher3/AppDrawerScrubberSections.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AppDrawerScrubberSections {
+    private static final String TAG = AppDrawerScrubber.class.getSimpleName();
+    private static final String ALPHA_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int MAX_NUMBER_CUSTOM_HEADERS = 8;
+    private static final int MAX_HEADERS = ALPHA_LETTERS.length() + MAX_NUMBER_CUSTOM_HEADERS;
+    public static final int INVALID_INDEX = -1;
+
+    private AutoExpandTextView.HighlightedText mHighlightedText;
+    private int mPreviousValidIndex;
+    private int mNextValidIndex;
+    private int mAdapterIndex;
+
+    public AppDrawerScrubberSections(String text, boolean highlight, int idx) {
+        mHighlightedText = new AutoExpandTextView.HighlightedText(text, highlight);
+        mAdapterIndex = idx;
+        mPreviousValidIndex = mNextValidIndex = idx;
+    }
+
+    public boolean getHighlight() {
+        return mHighlightedText.mHighlight;
+    }
+
+    public String getText() {
+        return mHighlightedText.mText;
+    }
+
+    public int getPreviousIndex() {
+        return mPreviousValidIndex;
+    }
+
+    public int getNextIndex() {
+        return mNextValidIndex;
+    }
+
+    public int getAdapterIndex() {
+        return mAdapterIndex;
+    }
+
+    private static int getFirstValidIndex(ArrayList<AppDrawerScrubberSections> sections) {
+        for (int i = 0; i < sections.size(); i++) {
+            if (sections.get(i).getHighlight()) {
+                return i;
+            }
+        }
+
+        return INVALID_INDEX;
+    }
+
+    private static void createIndices(ArrayList<AppDrawerScrubberSections> sections) {
+        if (sections == null || sections.size() == 0) {
+            return;
+        }
+
+        // walk forwards and fill out the previous valid index based on the previous highlight
+        int currentIdx = getFirstValidIndex(sections);
+        for (int i = 0; i < sections.size(); i++) {
+            if (sections.get(i).getHighlight()) {
+                currentIdx = i;
+            }
+
+            sections.get(i).mPreviousValidIndex = currentIdx;
+        }
+
+        // currentIdx should be now on the last valid index so walk back and fill the other way
+        for (int i = sections.size() - 1; i >= 0; i--) {
+            if (sections.get(i).getHighlight()) {
+                currentIdx = i;
+            }
+
+            sections.get(i).mNextValidIndex = currentIdx;
+        }
+    }
+
+    public static ArrayList<AutoExpandTextView.HighlightedText> getHighlightText(
+            ArrayList<AppDrawerScrubberSections> sections) {
+        if (sections == null) {
+            return null;
+        }
+
+        ArrayList<AutoExpandTextView.HighlightedText> highlights = new ArrayList<>(sections.size());
+        for (AppDrawerScrubberSections section : sections) {
+            highlights.add(section.mHighlightedText);
+        }
+
+        return highlights;
+    }
+
+    private static void addAlphaLetters(ArrayList<AppDrawerScrubberSections> sections,
+                                        HashMap<Integer, Integer> foundAlphaLetters) {
+        for (int i = 0; i < ALPHA_LETTERS.length(); i++) {
+            boolean highlighted = foundAlphaLetters.containsKey(i);
+            int index = highlighted
+                    ? foundAlphaLetters.get(i) : AppDrawerScrubberSections.INVALID_INDEX;
+
+            sections.add(new AppDrawerScrubberSections(ALPHA_LETTERS.substring(i, i + 1),
+                    highlighted, index));
+        }
+    }
+
+    /**
+     * Takes the headers and runs some checks to see if we can create a valid
+     * appDrawerScrubberSection out of it.  This list will contain the original header list plus
+     * fill out the remaining sections based on the ALPHA_LETTERS.  It will then determine which
+     * ones to highlight as well as what letters to highlight when scrolling over the
+     * grayed out sections
+     * @param headers list of header Strings
+     * @return the list of scrubber sections
+     */
+    public static ArrayList<AppDrawerScrubberSections> createSections(String[] headers) {
+        // check if we have a valid header section
+        if (!validHeaderList(headers)) {
+            return null;
+        }
+
+        // this will track the mapping of ALPHA_LETTERS index to the headers index
+        HashMap<Integer, Integer> foundAlphaLetters = new HashMap<>();
+        ArrayList<AppDrawerScrubberSections> sections = new ArrayList<>(headers.length);
+        boolean inAlphaLetterSection = false;
+
+        for (int i = 0; i < headers.length; i++) {
+            int alphaLetterIndex = TextUtils.isEmpty(headers[i])
+                    ? -1 : ALPHA_LETTERS.indexOf(headers[i]);
+
+            // if we found an ALPHA_LETTERS store that in foundAlphaLetters and continue
+            if (alphaLetterIndex >= 0) {
+                foundAlphaLetters.put(alphaLetterIndex, i);
+                inAlphaLetterSection = true;
+            } else {
+                // if we are exiting the ALPHA_LETTERS section, add it here
+                if (inAlphaLetterSection) {
+                    addAlphaLetters(sections, foundAlphaLetters);
+                    inAlphaLetterSection = false;
+                }
+
+                // add the custom header
+                sections.add(new AppDrawerScrubberSections(headers[i], true, i));
+            }
+        }
+
+        // if the last section are the alpha letters, then add it
+        if (inAlphaLetterSection) {
+            addAlphaLetters(sections, foundAlphaLetters);
+        }
+
+        // create the forward and backwards indices for scrolling over the grayed out sections
+        AppDrawerScrubberSections.createIndices(sections);
+
+        return sections;
+    }
+
+    /**
+     * Walk through the headers and check for a few things:
+     * 1) No more than MAX_NUMBER_CUSTOM_HEADERS headers exist in the headers list or no more
+     * than MAX_HEADERS headers exist in the list
+     * 2) the headers that fall in the ALPHA_LETTERS category are in the same order as ALPHA_LETTERS
+     * 3) There are no headers that exceed length of 1
+     * 4) The alpha letter section is together and not separated by other things
+     */
+    private static boolean validHeaderList(String[] headers) {
+        int numCustomHeaders = 0;
+        int previousAlphaIndex = -1;
+        boolean foundAlphaHeaders = false;
+
+        for (String s : headers) {
+            if (TextUtils.isEmpty(s)) {
+                numCustomHeaders++;
+                continue;
+            }
+
+            if (s.length() > 1) {
+                Log.w(TAG, "Found header " + s + " with length: " + s.length());
+                return false;
+            }
+
+            int alphaIndex = ALPHA_LETTERS.indexOf(s);
+            if (alphaIndex >= 0) {
+                if (previousAlphaIndex != -1) {
+                    // if the previous alpha index is >= alphaIndex then it is in the wrong order
+                    if (previousAlphaIndex >= alphaIndex) {
+                        Log.w(TAG, "Found letter index " + previousAlphaIndex
+                                + " which is greater than " + alphaIndex);
+                        return false;
+                    }
+                }
+
+                // if we've found headers previously and the index is -1 that means the alpha
+                // letters are separated out into two sections so return false
+                if (foundAlphaHeaders && previousAlphaIndex == -1) {
+                    Log.w(TAG, "Found alpha letters twice");
+                    return false;
+                }
+
+                previousAlphaIndex = alphaIndex;
+                foundAlphaHeaders = true;
+            } else {
+                numCustomHeaders++;
+                previousAlphaIndex = -1;
+            }
+        }
+
+        final int listSize = foundAlphaHeaders
+                ? numCustomHeaders + ALPHA_LETTERS.length()
+                : numCustomHeaders;
+
+        // if one of these conditions are satisfied, then return true
+        if (numCustomHeaders <= MAX_NUMBER_CUSTOM_HEADERS || listSize <= MAX_HEADERS) {
+            return true;
+        }
+
+        if (numCustomHeaders > MAX_NUMBER_CUSTOM_HEADERS) {
+            Log.w(TAG, "Found " + numCustomHeaders + "# custom headers when " +
+                    MAX_NUMBER_CUSTOM_HEADERS + " is allowed!");
+        } else if (listSize > MAX_HEADERS) {
+            Log.w(TAG, "Found " + listSize + " headers when " +
+                    MAX_HEADERS + " is allowed!");
+        }
+
+        return false;
+    }
+}
