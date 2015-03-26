f944e7e9eead78c7efe1823c8839b2a10d92fbb3
Raj Yengisetty <rajesh@cyngn.com>
2015-03-26 09:28:56 -0700

    ProtectedApps: map items without componentNames by position
    Repro:
     - Add a contact widget to a folder
     - Open the hidden apps fragment
     - Attempt to remove an entry from the fragment
     - Observe: Force close
    
    Change-Id: I3ca2c62283af5232534b766d7579413c060a5403
    (cherry picked from commit f963b11485ceef70bbce7a9a4b98415ac2672b5b)
    (cherry picked from commit 660761014f5ab0d6ebada8a1579db4da910643fe)
    (cherry picked from commit 8055e99967a786545f55ea4b3f4351583ee7f7b1)

--- src/com/android/launcher3/Folder.java
+++ src/com/android/launcher3/Folder.java
@@ -1490,6 +1490,18 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         return null;
     }
 
+    public ShortcutInfo getShortcutForPosition(int position) {
+        if (position < 0 || position >= mItemsInReadingOrder.size()) {
+            return null;
+        }
+        View v = mItemsInReadingOrder.get(position);
+        Object tag = v.getTag();
+        if (tag instanceof ShortcutInfo) {
+            return (ShortcutInfo) tag;
+        }
+        return null;
+    }
+
     public void getLocationInDragLayer(int[] loc) {
         mLauncher.getDragLayer().getLocationInDragLayer(this, loc);
     }
