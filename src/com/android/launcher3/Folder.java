22e21ad7a891988adad36453cc30a460da4cadb8
Raj Yengisetty <rajesh@cyngn.com>
2015-04-27 18:33:06 -0700

    Trebuchet: limit folder size to numRowsBase and numColumnsBase
    Otherwise the size of the folder is determined by the size of the
    Dynamic Grid. This causes some weird issues with sizes and scrolling
    so just cap this to the base values for the numRows and
    numColumns.
    
    Change-Id: I5b1f13267299d3ab1f28e6e393d6260fa7fbdd3e

--- src/com/android/launcher3/Folder.java
+++ src/com/android/launcher3/Folder.java
@@ -166,12 +166,12 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         mIconCache = app.getIconCache();
 
         Resources res = getResources();
-        mMaxCountX = (int) grid.numColumns;
+        mMaxCountX = (int) grid.numColumnsBase;
         // Allow scrolling folders when DISABLE_ALL_APPS is true.
         if (LauncherAppState.isDisableAllApps()) {
             mMaxCountY = mMaxNumItems = Integer.MAX_VALUE;
         } else {
-            mMaxCountY = (int) grid.numRows;
+            mMaxCountY = (int) grid.numRowsBase;
             mMaxNumItems = mMaxCountX * mMaxCountY;
         }
 
