59ca19ff68b20a6dc80014c21dff29a835afb91b
Stephen Bird <sbird@cyngn.com>
2015-06-11 10:14:11 -0700

    Rotation: fix icons disappearing
    Only the dynamic grid reload should be
    stopping the launcher with intent to rebuild
    the entire workspace.
    
    This returns stopLoader to it's functionality
    pre-dynamic grid resize patch.
    
    Change-Id: I55861da83f9b640fd767031e0f2188c5b9b91e66
    (cherry picked from commit 7d006588505d1ccc1a04cf9080851f0a7abeca66)
    (cherry picked from commit 36fcac12c25d0cc733ed0d4a90889d140434fd07)

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -5824,7 +5824,7 @@ public class Launcher extends Activity
         mGrid.layout(Launcher.this);
 
         // Synchronized reload
-        mModel.stopLoader(); //make sure the loader isn't running
+        mModel.resetLoadedState(true, true);
         mModel.startLoader(true, page);
         mWorkspace.updateCustomContentVisibility();
 
