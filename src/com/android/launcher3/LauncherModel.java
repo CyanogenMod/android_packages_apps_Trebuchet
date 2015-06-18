34dbd4ad06b87fafec5b2bd2745eb283018d1bcf
d34d <clark@cyngn.com>
2015-06-18 15:08:41 -0700

    Don't load title from intent if title loaded from DB
    Title for shortcuts, when creating workspaces, are loaded from the
    intent rather than the title stored in the DB.  The title from the
    DB is loaded in the ShortcutInfo so we will use that unless it is
    null, in which case we'll fallback to loading the title from the
    intent.
    
    Change-Id: I436c11062dbc8ff3d7e1cd9f9564321ad0476933
    (cherry picked from commit e963932853caa238ba7bc6928e5243075e41fc75)

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -2256,9 +2256,11 @@ public class LauncherModel extends BroadcastReceiver
                                             iconPackageIndex, iconResourceIndex, iconIndex,
                                             titleIndex);
 
-                                    CharSequence title = getShortcutTitle(manager, intent);
-                                    if (title != null) {
-                                        info.title = title;
+                                    if (info.title == null) {
+                                        CharSequence title = getShortcutTitle(manager, intent);
+                                        if (title != null) {
+                                            info.title = title;
+                                        }
                                     }
 
                                     // App shortcuts that used to be automatically added to Launcher
