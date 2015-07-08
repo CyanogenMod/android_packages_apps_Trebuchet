1cecd2226228422df31ebaf90c2c21172d965b50
Stephen Bird <sbird@cyngn.com>
2015-07-07 18:37:40 -0700

    Trebuchet: fix voice button proxy
    Change-Id: I1f7243063fa0596f5f9cb3b40c4cee63545d04e3
    (cherry picked from commit a433e1da44fdc487cef01c5136e9ce8029eaff2f)
    (cherry picked from commit 004115fe4b3abbf27115ebbde18473353b405407)

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -338,6 +338,8 @@ public class Launcher extends Activity
     private boolean mHasFocus = false;
     private boolean mAttached = false;
 
+    private boolean mShouldShowVoice = false;
+
     private static LocaleConfiguration sLocaleConfiguration = null;
 
     private static HashMap<Long, FolderInfo> sFolders = new HashMap<Long, FolderInfo>();
@@ -4407,6 +4409,7 @@ public class Launcher extends Activity
             if (voiceButtonContainer != null) voiceButtonContainer.setVisibility(View.GONE);
             if (searchButton != null) searchButton.setVisibility(View.GONE);
             if (voiceButton != null) voiceButton.setVisibility(View.GONE);
+            mShouldShowVoice = false;
             updateVoiceButtonProxyVisible(true);
             return false;
         }
@@ -4456,10 +4459,12 @@ public class Launcher extends Activity
             voiceButton.setVisibility(View.VISIBLE);
             updateVoiceButtonProxyVisible(false);
             invalidatePressedFocusedStates(voiceButtonContainer, voiceButton);
+            mShouldShowVoice = true;
             return true;
         } else {
             if (voiceButtonContainer != null) voiceButtonContainer.setVisibility(View.GONE);
             if (voiceButton != null) voiceButton.setVisibility(View.GONE);
+            mShouldShowVoice = false;
             updateVoiceButtonProxyVisible(true);
             return false;
         }
@@ -4476,7 +4481,7 @@ public class Launcher extends Activity
         final View voiceButtonProxy = findViewById(R.id.voice_button_proxy);
         if (voiceButtonProxy != null) {
             boolean visible = !forceDisableVoiceButtonProxy &&
-                    mWorkspace.shouldVoiceButtonProxyBeVisible();
+                    mWorkspace.shouldVoiceButtonProxyBeVisible() && mShouldShowVoice;
             voiceButtonProxy.setVisibility(visible ? View.VISIBLE : View.GONE);
             voiceButtonProxy.bringToFront();
         }
