0b93e24e66ff267e304e8977f7dde966cc9ec95b
Raj Yengisetty <rajesh@cyngn.com>
2015-07-14 17:57:53 -0700

    Loader: use INVALID_RESTORE_PAGE to avoid unnecessarily synchronizing
    Change-Id: I55abe9e6dd58ce422270c640fdd900185422f704

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -1650,7 +1650,7 @@ public class LauncherModel extends BroadcastReceiver
             }
 
             // Bind the workspace
-            bindWorkspace(-1, isUpgradePath);
+            bindWorkspace(PagedView.INVALID_RESTORE_PAGE, isUpgradePath);
             return isUpgradePath;
         }
 
