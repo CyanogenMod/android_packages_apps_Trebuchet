547e58943c941931561f44b9c2627fa13906e15d
Ricardo Cerqueira <ricardo@cyngn.com>
2015-08-31 16:02:12 +0100

    Support STK app rename on MSIM
    Companion change to If64b278360a0b8433e23e6678f04cdf30fe2cef1
    
    Change-Id: I36c74301c7813e6c118faed9fa5b31a36233db1e

--- src/com/android/launcher3/IconCache.java
+++ src/com/android/launcher3/IconCache.java
@@ -336,10 +336,12 @@ public class IconCache {
                 getBoolean(R.bool.config_launcher_stkAppRename))
                 && info.getComponentName().getPackageName().toString()
                         .equalsIgnoreCase(STK_PACKAGE_NAME);
+        String activity = info.getComponentName().getClassName().toString();
+
         boolean isCustomTitle = false;
         if (condition
                 && !TextUtils.isEmpty(((LauncherApplication) mContext)
-                        .getStkAppName())) {
+                        .getStkAppName(activity))) {
             isCustomTitle = true;
         }
         if (entry == null || unreadNum >= 0) {
@@ -352,14 +354,14 @@ public class IconCache {
                 if (labelCache != null && labelCache.containsKey(labelKey)) {
                     if (isCustomTitle) {
                         entry.title = ((LauncherApplication) mContext)
-                                .getStkAppName();
+                                .getStkAppName(activity);
                     } else {
                         entry.title = labelCache.get(labelKey).toString();
                     }
                 } else {
                     if (isCustomTitle) {
                         entry.title = ((LauncherApplication) mContext)
-                                .getStkAppName();
+                                .getStkAppName(activity);
                     } else {
                         entry.title = info.getLabel().toString();
                     }
