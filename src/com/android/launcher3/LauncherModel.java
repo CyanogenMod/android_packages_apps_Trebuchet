03b202e6b17e0b6be94eb70606e2bcb9a4f6e090
Adnan Begovic <adnan@cyngn.com>
2015-09-11 14:15:10 -0700

    Trebuchet: Make sure callbacks are available on user change.
      When switching from Owner -> Guest profile, the callbacks for
      LauncherModel may not be supplied by the incoming restricted
      user.
    
    Change-Id: I15c49f3f30fea9397bee5ae8bde4b0c7f585ae07
    TICKET: CYNGNOS-997

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -1875,7 +1875,7 @@ public class LauncherModel extends BroadcastReceiver
             long containerIndex = item.screenId;
             if (item.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
                 // Return early if we detect that an item is under the hotseat button
-                if (mCallbacks == null ||
+                if (mCallbacks == null || mCallbacks.get() == null ||
                         mCallbacks.get().isAllAppsButtonRank((int) item.screenId)) {
                     deleteOnInvalidPlacement.set(true);
                     Log.e(TAG, "Error loading shortcut into hotseat " + item
