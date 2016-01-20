c9018137a482bd0e3e997fc457864b266d9616fb
Linus Lee <llee@cyngn.com>
2016-01-19 16:41:19 -0800

    fix CTS failure on Trebuchet - manually picked from a269d518db02c8383e3621dff9d6054e3e9e48d6
    From: Liu Zongyu <zongyu@codeaurora.org>
    Date: Thu, 10 Dec 2015 12:46:33 +0800
    Subject: Trebuchet: fix CTS test failed
    
    The searchManager api was changed, we can't get the search info.
    
    Modify the code, we get the component name from packagemanager.
    
    CRs-fixed: 948706
    OPO-336
    
    Change-Id: I641ba64036994781bc15e5d6e0955deb9fec13ed
    (cherry picked from commit f15a377ed8914e3dbc47fcc3118f072899277220)

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -532,9 +532,14 @@ public final class Utilities {
      */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     public static AppWidgetProviderInfo getSearchWidgetProvider(Context context) {
-        SearchManager searchManager =
-                (SearchManager) context.getSystemService(Context.SEARCH_SERVICE);
-        ComponentName searchComponent = searchManager.getGlobalSearchActivity();
+        ComponentName searchComponent = null;
+        try {
+            Intent assistIntent = new Intent(Intent.ACTION_ASSIST);
+            searchComponent = assistIntent.resolveActivity(context.getPackageManager());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in resolveSearchAppWidget: " + e);
+        }
+
         if (searchComponent == null) return null;
         String providerPkg = searchComponent.getPackageName();
 
