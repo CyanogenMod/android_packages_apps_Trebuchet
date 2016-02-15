577d34207e3d9fcd676d093019bd85da631cef8b
cretin45 <cretin45@gmail.com>
2016-02-15 09:20:04 -0800

    Trebuchet: Fix measuring error when reloading app drawer
    Issue-id: CYNGNOS-1883
    
    Change-Id: I77705be7020120bc58a45f507b288838a5eba2bb

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1885,7 +1885,7 @@ public class Launcher extends Activity
         setupSearchBar(this);
         mAppsView.addApps(addedApps);
         tryAndUpdatePredictedApps();
-        mAppsView.reset();
+        mAppsView.onReloadAppDrawer();
         sRemoteFolderManager.onReloadAppDrawer();
     }
 
