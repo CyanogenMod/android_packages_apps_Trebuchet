681f47db087a5ccc9acef54352fd916d9ce23548
cretin45 <cretin45@gmail.com>
2016-03-24 11:37:44 -0700

    Trebuchet: Fix NPE while binding unavailable items
    Issue-id: CYNGNOS-2282
    
    Change-Id: I6895c3c043a2b8a9506f3dc2f8a357942d8f35c4
    (cherry picked from commit e812052ff07a96e5008fdfea671357c74baf21e3)

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -4375,7 +4375,9 @@ public class Workspace extends PagedView
                 final int N = folderPagedView.getItemCount();
                 for (int page = 0; page < N; page++) {
                     final CellLayout cellLayout = folderPagedView.getPageAt(page);
-                    updateUnvailableItemsInCellLayout(cellLayout, packages);
+                    if (cellLayout != null) {
+                        updateUnvailableItemsInCellLayout(cellLayout, packages);
+                    }
                 }
                 folder.invalidate();
             }
