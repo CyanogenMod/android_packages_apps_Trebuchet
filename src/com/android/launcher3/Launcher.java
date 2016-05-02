7657eae25cac0aef261c436a102a18913f16f46a
Artem Shvadskiy <ashvadskiy@cyngn.com>
2016-05-02 12:45:46 -0700

    Prevent crash when exiting grid size fragment
    Change-Id: Iea39713b71a8c54bf4bf79f0b1b0c865a7950fa4
    issue-id: FEIJ-197
    (cherry picked from commit a56a75dbeb84341ba1450fa58bad63301201aae9)

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1215,7 +1215,7 @@ public class Launcher extends Activity
         Fragment gridFragment = getFragmentManager().findFragmentByTag(
                 DynamicGridSizeFragment.DYNAMIC_GRID_SIZE_FRAGMENT);
         if (gridFragment != null) {
-            mDynamicGridSizeFragment.setSize();
+            ((DynamicGridSizeFragment) gridFragment).setSize();
             unlockScreenOrientation(true);
         }
     }
@@ -1928,6 +1928,8 @@ public class Launcher extends Activity
      * @param size The new grid size to set the workspace to.
      */
     public void setDynamicGridSize(InvariantDeviceProfile.GridSize size) {
+        if (size == null) return;
+
         int gridSize = SettingsProvider.getIntCustomDefault(this,
                 SettingsProvider.SETTINGS_UI_DYNAMIC_GRID_SIZE, 0);
         boolean customValuesChanged = false;
@@ -2815,7 +2817,7 @@ public class Launcher extends Activity
             Fragment gridFragment = getFragmentManager().findFragmentByTag(
                     DynamicGridSizeFragment.DYNAMIC_GRID_SIZE_FRAGMENT);
             if (gridFragment != null) {
-                mDynamicGridSizeFragment.setSize();
+                ((DynamicGridSizeFragment) gridFragment).setSize();
                 unlockScreenOrientation(true);
             }
             else {
